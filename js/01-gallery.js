980e7b23a469dcceab43cf08efed4b290899e958
Anatoliia Riabchenko <108002173+AnatoliiaR@users.noreply.github.com>
2022-11-21 13:33:15 +0200

    22

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+const GalleryRefs = document.querySelector('.gallery');
+
+
+
+
+
+function CreateGalerry(item) {
+    return item.map(({ preview, original, description }) => {
+        
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`
+            ;
+    }).join('');
+};
+// console.log(CreateGalerry(galleryItems));
+
+const GalleryMarkup = CreateGalerry(galleryItems);
+GalleryRefs.insertAdjacentHTML('beforeend', GalleryMarkup);
+
+
+
+
+const onItemClick = (e) => {
+
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src="${e.target.dataset.source}" width="800" height="600">`
+    );
+    instance.show();
+}
+GalleryRefs.addEventListener('click', onItemClick);
+
+
+
+
+
